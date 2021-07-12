06b5241179ef36388a0acf22bd4ff48e2df299b2
Keith Post <kpost342005@hotmail.com>
2021-07-12 14:33:12 -0600

    Updated, working code for shiny app

--- t_tester_app/server.R
+++ t_tester_app/server.R
@@ -1,26 +1,46 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+# Define server logic required to run a t-test and draw a barplot
+shinyServer(function(input, output,session) {
+        sample1<-reactive({rnorm(n=input$n_samp1,mean=input$mean_samp1,sd=input$sd1)}) #creates reactive expression based on UI inputs; sample1 is random draw from N dist
+        sample2<-reactive({rnorm(n=input$n_samp2,mean=input$mean_samp2,sd=input$sd2)})
+        t_test<-reactive({t.test(sample1(),sample2())})#runs t-test
+    observeEvent(input$submit_button,{ #observeEvent allows outputs to change when submit button is clicked
+        output$t_val<-renderText({ #extracts the t-value from t-test and outputs to UI
+            isolate(paste("t-value: ", round(t_test()$statistic,3))) #isolate() tells Shiny to calculate only when submit button is clicked
+        })
+        output$p_val<-renderText({
+            isolate(paste("p-value: ", round(t_test()$p.value,5)))
+        })
+        output$CI<-renderText({
+            isolate(paste0("95% confidence interval: (",  #paste0 function 
+                  round(t_test()$conf.int[1],3),
+                  ", ",
+                  round(t_test()$conf.int[2],3),
+                  ")"))
+        })
     })
-
+    
+    observeEvent(input$submit_button,{
+        output$barplot<-renderPlot({
+            isolate(samp1<-tibble(Sample=rep("s1",input$n_samp1),Value=sample1()))
+            isolate(samp2<-tibble(Sample=rep("s2",input$n_samp2),Value=sample2()))
+            isolate(shiny_data<-bind_rows(samp1,samp2))
+            isolate(ggplot(shiny_data,aes(Sample,Value)) + 
+                geom_bar(stat="summary", width=0.5,fill="steel blue") + 
+                geom_errorbar(stat="summary",width=0.2) +
+                labs(x="Sample",y="Mean +- 1 SE")) +
+                scale_y_continuous(expand=expansion(mult=c(0,.1))) +
+                theme_classic(base_size=20)
+            })
+        })
 })
+
+       
+
+
+
+
--- t_tester_app/ui.R
+++ t_tester_app/ui.R
@@ -1,33 +1,49 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a bar plot and runs a t-test
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("t-test Shiny Application"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar with numeric inputs for mean and n's and slidrs for st devs
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
+            numericInput("n_samp1","Enter the size of sample 1",value=0), #first of four text boxes for n's and means
+            numericInput("n_samp2","Enter the size of sample 2",value=0),
+            numericInput("mean_samp1","Mean of population 1",value=0),
+            numericInput("mean_samp2","Mean of population 2",value=0),
+            sliderInput("sd1","Standard deviation of population 1",min=0.5,max=20,value=1,step=0.5), #slider input for pop1
+            sliderInput("sd2","Standard deviation of population 2",min=0.5,max=20,value=1,step=0.5),
+            actionButton("submit_button","Submit")), #creates submit button
+            
+        # Show a plot of the generated distribution and return t-test stats
         mainPanel(
-            plotOutput("distPlot")
+            tabsetPanel(type="tabs", #creates tabs: 1) stats and bar graph and 2) info and link to code
+                tabPanel("Stats and barplot", #first tab
+                    tags$br(), #inserts line break
+                    textOutput("t_val"), #outputs t-statistic from t-test
+                    textOutput("p_val"), #p-value
+                    textOutput("CI"), #confidence interval
+                    plotOutput("barplot")), #plots barplot
+                tabPanel("Instructions and Code", tags$p( #second tab; tags$p creates a paragraph
+                    tags$br(), #line break
+                    "This Shiny application allows a user to perform an unpaired t-test on samples pulled from
+                    normal distributions. Their sample sizes and the means and standard deviations of their populations
+                    can be specified using the text boxes and sliders on the left pane. Clicking on the submit button
+                    returns the t-statistic, p-value, and 95% confidence interval as well as a barplot of sample
+                    means with standard errors. Adjust the inputs to understand how they impact the test
+                    statistics and distributions of samples.",
+                    tags$br(), #line break separates background info (above) and sentence below about code link
+                    tags$br(), #creates line spacde
+                    "If you would like to see the code for this Shiny app, 
+                    please visit my",
+                    tags$a(href="https://github.com/kpost34/t_tester_shiny_app", #tags$a creates a hyperlink to Github
+                    "Github repo"),".") 
+                )
+            )
         )
     )
 ))
+
+
